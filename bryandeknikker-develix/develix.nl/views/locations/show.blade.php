3a4b0073348922ba38255128cf6d1313042cd6c5
Bryan0182 <bdeknikker04@gmail.com>
2024-11-09 20:00:12 +0100

    Location pages

--- bryandeknikker-develix/develix.nl/views/locations/show.blade.php
+++ bryandeknikker-develix/develix.nl/views/locations/show.blade.php
@@ -3,12 +3,6 @@
 @section('title', "Website laten maken in " . $location->location)
 
 @section('content')
-    <div class="container mx-auto py-5 px-4">
-        <h1 class="text-4xl font-bold mb-4">Website laten maken in {{ $location->location }}</h1>
-        <p>Welkom bij Develix! Wij helpen bedrijven in {{ $location->location }} met het ontwikkelen van professionele websites die perfect aansluiten bij hun wensen en doelen.</p>
-
-        <p>Ons team zorgt ervoor dat uw bedrijf in {{ $location->location }} online optimaal zichtbaar is.</p>
-
-        <p>Neem vandaag nog contact op voor uw project in {{ $location->location }}!</p>
-    </div>
+    @component('components.location-show', ['location' => $location])
+    @endcomponent
 @endsection
---
+++ bryandeknikker-develix/resources/views/components/location-show.blade.php
@@ -0,0 +1,29 @@
+<div class="container mx-auto py-5 px-4">
+    <h1 class="text-4xl font-bold mb-4">Professionele Website Laten Maken in {{ $location->location }}</h1>
+
+    <p>Welkom bij Develix! Wij zijn gespecialiseerd in het maken van maatwerk websites voor bedrijven en ondernemers in {{ $location->location }} die streven naar een sterke online aanwezigheid en betere vindbaarheid.</p>
+
+    <h2 class="text-2xl font-semibold mt-6 mb-2">Waarom een Website Laten Maken in {{ $location->location }} met Develix?</h2>
+    <p>Bij Develix begrijpen we dat iedere onderneming uniek is. Ons team werkt nauw samen met bedrijven in {{ $location->location }} om websites te creëren die volledig aansluiten bij uw merk en bedrijfsdoelen. We zorgen ervoor dat uw website:</p>
+    <ul class="list-disc pl-6">
+        <li>Goed vindbaar is in zoekmachines zoals Google.</li>
+        <li>Een aantrekkelijke en gebruiksvriendelijke uitstraling heeft.</li>
+        <li>Geoptimaliseerd is voor mobiele apparaten en snelle laadtijden.</li>
+        <li>Voorzien is van sterke SEO-optimalisatie voor {{ $location->location }}.</li>
+    </ul>
+
+    <h2 class="text-2xl font-semibold mt-6 mb-2">Onze Diensten in {{ $location->location }}</h2>
+    <p>Wij bieden een breed scala aan diensten voor bedrijven in {{ $location->location }}, waaronder:</p>
+    <ul class="list-disc pl-6">
+        <li>Webdesign en websiteontwikkeling</li>
+        <li>SEO en zoekmachineoptimalisatie</li>
+        <li>Contentbeheer en onderhoud</li>
+        <li>Hosting en beveiligingsservices</li>
+    </ul>
+
+    <h2 class="text-2xl font-semibold mt-6 mb-2">Uw Project in {{ $location->location }} Starten?</h2>
+    <p>Wilt u een professionele website laten maken voor uw bedrijf in {{ $location->location }}? Neem vandaag nog contact op met Develix om de mogelijkheden te bespreken. Ons team helpt u graag om uw online doelen te bereiken!</p>
+
+    <h3 class="text-xl font-semibold mt-6 mb-2">Contactinformatie</h3>
+    <p>Voor meer informatie over onze diensten in {{ $location->location }}, kunt u ons altijd bereiken via onze <a href="{{ route('contact') }}" class="link-contact">contactpagina</a>. Wij kijken ernaar uit om uw bedrijf in {{ $location->location }} te helpen met een op maat gemaakte website die resultaten levert.</p>
+</div>
