9a498ce249292b53e74fd7e378838ec3e01521cb
Bryan0182 <bdeknikker04@gmail.com>
2024-11-11 08:40:02 +0100

    Changes for pages and added cookies

--- bryandeknikker-develix/resources/js/develix.nl/cookie-element.js
+++ bryandeknikker-develix/resources/js/develix.nl/cookie-element.js
@@ -2,24 +2,20 @@ window.acceptCookies = function() {
     setCookie("cookieAccepted", "true", 365);
     document.getElementById("cookieConsent").style.display = "none";
 
-    // Voeg GTM-script toe aan de head na toestemming
     var gtmScript = document.createElement("script");
     gtmScript.async = true;
     gtmScript.src = "https://www.googletagmanager.com/gtm.js?id=GTM-T95BJNHF";
     document.head.appendChild(gtmScript);
 
-    // Initialiseer dataLayer en stuur een start event naar GTM
     window.dataLayer = window.dataLayer || [];
     window.dataLayer.push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
     window.dataLayer.push({ event: 'cookieConsentGranted' });
 
-    // Voeg dynamisch het noscript iframe toe aan de <body>
     var noscript = document.createElement("noscript");
     noscript.innerHTML = '<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T95BJNHF" height="0" width="0" style="display:none;visibility:hidden"></iframe>';
     document.body.appendChild(noscript);
 };
 
-// Functie om een cookie in te stellen
 function setCookie(name, value, days) {
     var expires = "";
     if (days) {
@@ -30,7 +26,6 @@ function setCookie(name, value, days) {
     document.cookie = name + "=" + (value || "") + expires + "; path=/";
 }
 
-// Functie om een cookie op te halen
 function getCookie(name) {
     var nameEQ = name + "=";
     var ca = document.cookie.split(';');
@@ -42,25 +37,20 @@ function getCookie(name) {
     return null;
 }
 
-// Controleer bij pagina-lading of de cookie al bestaat en laad GTM indien nodig
 document.addEventListener("DOMContentLoaded", function () {
     if (getCookie("cookieAccepted")) {
-        // Als cookie al bestaat, laad het GTM-script automatisch
         var gtmScript = document.createElement("script");
         gtmScript.async = true;
         gtmScript.src = "https://www.googletagmanager.com/gtm.js?id=GTM-T95BJNHF";
         document.head.appendChild(gtmScript);
 
-        // Initialiseer dataLayer en stuur start event naar GTM
         window.dataLayer = window.dataLayer || [];
         window.dataLayer.push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
 
-        // Voeg dynamisch het noscript iframe toe aan de <body>
         var noscript = document.createElement("noscript");
         noscript.innerHTML = '<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T95BJNHF" height="0" width="0" style="display:none;visibility:hidden"></iframe>';
         document.body.appendChild(noscript);
     } else {
-        // Toon cookiemelding als cookie nog niet bestaat
         document.getElementById("cookieConsent").style.display = "block";
     }
 });
