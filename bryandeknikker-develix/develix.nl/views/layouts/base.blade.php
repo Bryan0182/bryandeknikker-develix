b2ef1dfafc0ed7b4142673a15502646223615c25
Bryan0182 <bdeknikker04@gmail.com>
2024-11-23 14:55:04 +0100

    Added GTM

--- bryandeknikker-develix/develix.nl/views/layouts/base.blade.php
+++ bryandeknikker-develix/develix.nl/views/layouts/base.blade.php
@@ -6,11 +6,24 @@
     <title>@yield('title', 'Develix') - Develix</title>
     <link rel="icon" href="{{ asset('images/develix.nl/develix.svg') }}" type="image/x-icon">
 
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-T95BJNHF');</script>
+    <!-- End Google Tag Manager -->
+
     @vite(['resources/scss/global/app.scss', 'resources/scss/develix.nl/develix.scss', 'resources/scss/develix.nl/header.scss', 'resources/scss/develix.nl/footer.scss', 'resources/scss/develix.nl/cookie-element.scss', 'resources/scss/develix.nl/buttons.scss'])
 
     @yield('page-specific-scss')
 </head>
 <body>
+
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T95BJNHF"
+                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 
 @include('develix::partials.header')
 
