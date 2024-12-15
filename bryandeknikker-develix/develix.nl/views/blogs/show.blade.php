9f4741acd7cefd800f85752a6ff3f39235f58047
Bryan0182 <bdeknikker04@gmail.com>
2024-12-15 14:42:45 +0100

    Changes for blogs

--- bryandeknikker-develix/develix.nl/views/blogs/show.blade.php
+++ bryandeknikker-develix/develix.nl/views/blogs/show.blade.php
@@ -1,6 +1,13 @@
 @extends('develix::layouts.base')
 
 @section('title', $blog->title)
+@section('subtitle', $blog->subtitle ?? 'Jouw digitale partner voor groei')
+@section('meta_description', $blog->meta_description ?? 'Ontdek hoe Develix bedrijven helpt groeien met maatwerk digitale oplossingen.')
+@section('meta_keywords', 'Over Develix, digitale partner, webontwikkeling, SEO, maatwerk oplossingen')
+@section('og_title', $blog->meta_title ?? $blog->title)
+@section('og_description', $blog->meta_description ?? 'Bij Develix draait alles om kwaliteit, klantgerichtheid en innovatie. Ontdek hoe wij bedrijven helpen groeien met maatwerk digitale oplossingen.')
+@section('og_image', $blog->featured_image ? asset('storage/' . $blog->featured_image) : asset('images/develix.nl/operating-system-vector.svg'))
+@section('canonical', route('blogs.show', $blog->slug))
 
 @section('page-specific-scss')
     @vite(['resources/scss/global/blog-show.scss'])
