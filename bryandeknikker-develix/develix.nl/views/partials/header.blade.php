f85f70da27c81ce67620ebe61374117834fda4ff
Bryan0182 <bdeknikker04@gmail.com>
2025-01-03 17:11:28 +0100

    Changes

--- bryandeknikker-develix/develix.nl/views/partials/header.blade.php
+++ bryandeknikker-develix/develix.nl/views/partials/header.blade.php
@@ -13,7 +13,6 @@
         <!-- Navigatiebalk (desktop) -->
         <nav id="menu" class="space-x-4 hidden lg:flex">
             <div class="relative group">
-                <a href="{{ route('diensten') }}" class="focus:outline-none menu-item dropdown-switch">Diensten</a>
                 <a href="{{ route('services') }}" class="focus:outline-none menu-item dropdown-switch">Diensten</a>
                 <div id="dropdown-menu"
                      class="absolute hidden group-hover:block py-2 px-2 w-80">
@@ -106,7 +105,6 @@
         <div class="relative">
             <button id="mobile-dropdown-btn" class="block menu-item focus:outline-none dropdown-switch">Diensten</button>
             <div id="mobile-dropdown-menu" class="hidden space-y-2 pl-4">
-                <a href="{{ route('diensten') }}" class="block menu-item ">Diensten</a>
                 <a href="{{ route('services') }}" class="block menu-item ">Diensten</a>
                 <a href="{{ route('website') }}" class="block menu-item">Website</a>
                 <a href="{{ route('application') }}" class="block menu-item">Applicatie</a>
