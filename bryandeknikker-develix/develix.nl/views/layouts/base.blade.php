3548c466bdc626060ff9012d0b94035eb4f7bc65
Bryan0182 <bdeknikker04@gmail.com>
2025-01-03 17:07:37 +0100

    Added leadinfo

--- bryandeknikker-develix/develix.nl/views/layouts/base.blade.php
+++ bryandeknikker-develix/develix.nl/views/layouts/base.blade.php
@@ -19,7 +19,6 @@
     <!-- Canonical Tag -->
     <link rel="canonical" href="@yield('canonical', url()->current())">
 
-
     <!-- Favicon -->
     <link rel="icon" href="{{ asset('images/develix.nl/favicon.png') }}" type="image/png">
 
@@ -67,6 +66,14 @@
         })(window, document, "clarity", "script", "pdrpph9502");
     </script>
     <!-- End Microsoft Clarity -->
+
+    <!-- Leadinfo tracking code -->
+    <script>
+        (function(l,e,a,d,i,n,f,o){if(!l[i]){l.GlobalLeadinfoNamespace=l.GlobalLeadinfoNamespace||[];
+            l.GlobalLeadinfoNamespace.push(i);l[i]=function(){(l[i].q=l[i].q||[]).push(arguments)};l[i].t=l[i].t||n;
+            l[i].q=l[i].q||[];o=e.createElement(a);f=e.getElementsByTagName(a)[0];o.async=1;o.src=d;f.parentNode.insertBefore(o,f);}
+        }(window,document,'script','https://cdn.leadinfo.net/ping.js','leadinfo','LI-67780AEE837F5'));
+    </script>
 
     <!-- Styles -->
     @vite(['resources/scss/global/app.scss', 'resources/scss/develix.nl/develix.scss', 'resources/scss/develix.nl/header.scss', 'resources/scss/develix.nl/footer.scss', 'resources/scss/develix.nl/cookie-element.scss', 'resources/scss/develix.nl/buttons.scss'])
