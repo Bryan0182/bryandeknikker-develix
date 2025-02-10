09fdb59657b30de07416f42e7aa09deeb3a3df4d
Bryan0182 <bdeknikker04@gmail.com>
2025-02-10 18:59:34 +0100

    Changes for text block

--- bryandeknikker-develix/resources/views/components/text.blade.php
+++ bryandeknikker-develix/resources/views/components/text.blade.php
@@ -13,7 +13,7 @@
                 <div class="mt-6 flex flex-col items-center">
                     <div class="grid grid-cols-1 md:grid-cols-2 gap-x-16 gap-y-3 w-full">
                         @foreach ($usps as $index => $usp)
-                            <div class="flex items-center gap-3 {{ $index % 2 == 1 ? 'text-right ml-auto' : '' }}">
+                            <div class="flex items-center gap-3">
                                 <img src="https://develix.nl/images/develix.nl/check.svg"
                                      alt="Check"
                                      class="w-5 h-5">
