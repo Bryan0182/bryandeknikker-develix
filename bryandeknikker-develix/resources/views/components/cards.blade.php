ab186cd3b45ae1181c5d872d535ad3e7e065a3ad
Bryan0182 <bdeknikker04@gmail.com>
2025-02-16 16:01:25 +0100

    Changes for cards block

--- bryandeknikker-develix/resources/views/components/cards.blade.php
+++ bryandeknikker-develix/resources/views/components/cards.blade.php
@@ -6,7 +6,7 @@
         {{ $subtitle }}
     </p>
 
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         @foreach ($cards as $card)
             <div class="info-card px-4 py-2">
                 <div class="flex items-center justify-center h-12 w-12 rounded-full mb-4">
